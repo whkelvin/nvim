4f61c81fc52806b3329cfb5032391945f59e5308
whkelvin <whkelvin.hsu@gmail.com>
2022-06-29 19:20:00 -0300

    disable indent line in nvimtree

--- lua/plugins/configs/indent-blankline.lua
+++ lua/plugins/configs/indent-blankline.lua
@@ -4,24 +4,28 @@ if not present then
    return
 end
 
+vim.opt.list = true
+
 local options = {
-   indentLine_enabled = 1,
-   char = "▏",
-   filetype_exclude = {
-      "help",
-      "terminal",
-      "alpha",
-      "packer",
-      "lspinfo",
-      "TelescopePrompt",
-      "TelescopeResults",
-      "nvchad_cheatsheet",
-      "lsp-installer",
-      "",
-   },
-   buftype_exclude = { "terminal" },
-   show_trailing_blankline_indent = false,
-   show_first_indent_level = false,
+  --show_current_context = true,
+  space_char_blankline = " ",
+  indentLine_enabled = 1,
+  filetype_exclude = {
+    "help",
+    "terminal",
+    "alpha",
+    "packer",
+    "lspinfo",
+    "TelescopePrompt",
+    "TelescopeResults",
+    "nvchad_cheatsheet",
+    "lsp-installer",
+    "NvimTree",
+    "",
+  },
+  buftype_exclude = { "terminal" },
+  show_trailing_blankline_indent = false,
+  show_first_indent_level = false,
 }
 
 blankline.setup(options)
